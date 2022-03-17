b248f375e78cf824232f203d94ddff4df829c5f0
Ana Carolina Santos <anacarolina.s.s.hotmail.com>
2022-03-17 19:59:53 -0300

    Inicio do desenvolvimento dos recursos de gestão de instrutores e equipamentos

---
+++ src/br/com/senai/academiasenai/entity/Equipamento.java
@@ -0,0 +1,28 @@
+package br.com.senai.academiasenai.entity;
+
+public class Equipamento {
+	
+	private Integer id;
+
+	private String descricao;
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+
+}
