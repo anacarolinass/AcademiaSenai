19e09b1ddb515ef8d5562ae53e65cc87bad90ff6
Ana Carolina Santos <anacarolina.s.s.hotmail.com>
2022-03-17 21:23:15 -0300

    Adicionado novo atributo

--- src/br/com/senai/academiasenai/entity/Professor.java
+++ src/br/com/senai/academiasenai/entity/Professor.java
@@ -1,12 +1,23 @@
 package br.com.senai.academiasenai.entity;
 
+import java.time.LocalDate;
+
 public class Professor {
 
 	private Integer id;
 	
 	private String nomeCompleto;
 	
+	private LocalDate dataDeAdmissa;
 	
+
+	public LocalDate getDataDeAdmissa() {
+		return dataDeAdmissa;
+	}
+
+	public void setDataDeAdmissa(LocalDate dataDeAdmissa) {
+		this.dataDeAdmissa = dataDeAdmissa;
+	}
 
 	public Integer getId() {
 		return id;
